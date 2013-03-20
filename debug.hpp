7ef9181a43bdf61e0c9040c5e7c1be14eb6a1349
Régis Blanc <regwblanc@gmail.com>
2013-03-20 14:54:17 +0100

    new logging system

--- debug.hpp
+++ debug.hpp
@@ -1,10 +1,11 @@
-#ifndef _LOGGING_
-#define _LOGGING_
+#ifndef _DEBUG_
+#define _DEBUG_
 
 #include <iostream>
 #include <stdlib.h>
 
 #ifndef NASSERT
+
 #define ASSERT(expr) \
   if(!(expr)) {\
     std::cerr << "Assertion violation in file " << __FILE__ << " and line " << __LINE__ << std::endl;\
@@ -29,6 +30,14 @@
 #define ENSURE(expr) (void);
 //TODO
 #define INVARIANT(expr) (void);
+#endif
+
+#ifndef NDEBUG
+#define TRACE(fmt, ...) \
+  fprintf(stdout, "%s:%d:%s(): " fmt "\n", __FILE__, \
+          __LINE__, __func__, __VA_ARGS__);
+#else
+#define TRACE(fmt, ...) (void);
 #endif
 
 #endif
---
+++ logging.hpp
@@ -0,0 +1,28 @@
+#ifndef _LOGGING_
+#define _LOGGING_
+
+/*
+ * Logging is different from debugging.
+ * In debug.h we provide a TRACE macro that
+ * pretty much do the same as those logging
+ * macros but still has a different role. Tracing
+ * is meant to be low level and help during program development,
+ * and will be turned off in production, while logging will
+ * always be active.
+ */
+
+#define LOG_OUT stdout
+
+#define INFO(fmt, ...) \
+  fprintf(LOG_OUT, "[INFO] %s:%d:%s(): " fmt "\n", __FILE__, \
+          __LINE__, __func__, __VA_ARGS__);
+
+#define WARNING(fmt, ...) \
+  fprintf(LOG_OUT, "[WARNING] %s:%d:%s(): " fmt "\n", __FILE__, \
+          __LINE__, __func__, __VA_ARGS__);
+
+#define ERROR(fmt, ...) \
+  fprintf(LOG_OUT, "[ERROR] %s:%d:%s(): " fmt "\n", __FILE__, \
+          __LINE__, __func__, __VA_ARGS__);
+
+#endif
